209aecf99f88bda34b36403d5f6289f6c1712c02
Roger Cabral <roger_cabral@hotmail.com>
2021-12-18 19:02:03 -0300

    add express and get method

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const port = 3003;
+
+const express = require("express");
+const app = express();
+
+app.get("/produtos", (req, res, next) => {
+  res.send({ nome: "MacBook Air M1", preco: 12999.0 });
+});
+
+app.listen(port, () => {
+    console.log(`Server is now running on port ${port}`)
+});
