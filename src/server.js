94444db1043d80e94207630087fa6717f73fe111
Roger Cabral <roger_cabral@hotmail.com>
2021-12-18 19:54:09 -0300

    add body-parser

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const port = 3003;
 const express = require("express");
 const app = express();
 const database = require("./dataBase");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/products", (req, res, next) => {
   res.send(database.getProducts());
@@ -13,12 +17,12 @@ app.get("/products/:id", (req, res, next) => {
 });
 
 app.post("/products", (req, res, next) => {
-    const product = database.saveProduct({
-        name: req.body.name,
-        price: req.body.price
-    });
-    res.send(product)
-})
+  const product = database.saveProduct({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  res.send(product);
+});
 
 app.listen(port, () => {
   console.log(`Server is now running on port ${port}`);
